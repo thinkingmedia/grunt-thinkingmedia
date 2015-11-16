affd95ea181504a4d20bd63f60695d9a986c3396
thinkingmedia <mathew@thinkingmedia.ca>
2015-11-15 19:39:28 -0500

    added sass test

--- test/thinkingmedia_test.js
+++ test/thinkingmedia_test.js
@@ -1,15 +1,15 @@
 var grunt = require('grunt');
+var dircompare = require('dir-compare');
 
 exports.thinkingmedia = {
     setUp: function (done) {
         done();
     },
 
-    requiresDevOptions: function(test) {
-        test.done();
-    },
-
-    requiresProdOptions: function(test) {
+    sassDev: function (test) {
+        test.expect(1);
+        var res = dircompare.compareSync('test/expected/css', 'www/css');
+        test.equal(true, res.same, 'should match exclusions');
         test.done();
     }
 };
