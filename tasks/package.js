854d84d94afcafe565bf7551e1d796f2af840abc
thinkingmedia <mathew@thinkingmedia.ca>
2015-11-20 20:31:56 -0500

    updated package

--- tasks/package.js
+++ tasks/package.js
@@ -4,26 +4,20 @@ module.exports = function (grunt) {
 
     grunt.registerMultiTask('package', 'Handles the creation of minified JS/CSS files', function () {
         var self = this;
-        //var options = this.options();
 
-        //console.log('*********************************');
-        //console.log(self.data);
-        //console.log(self.filesSrc);
-        //console.log(self.files);
-        //console.log('*********************************');
-
+        // concat the files
         var src = _.map(self.filesSrc, function (filepath) {
-            //console.log(filepath);
             if (grunt.file.isDir(filepath)) {
                 return "";
             }
             return grunt.file.read(filepath);
         }).join("\n");
 
+        // save the concat version
         grunt.file.write(self.data.dest, src);
-
         grunt.log.writeln('File ' + self.data.dest + ' created.');
 
+        // clear unwanted files
         _.each(self.data.clear || [],function(filepath){
             grunt.file.delete(filepath);
         });
