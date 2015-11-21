a53fa8bb02e9f7d9084b504ac7249b87c39a194f
thinkingmedia <mathew@thinkingmedia.ca>
2015-11-20 20:56:43 -0500

    supports minify in package

--- tasks/package.js
+++ tasks/package.js
@@ -2,6 +2,9 @@ var _ = require('lodash');
 
 module.exports = function (grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     grunt.registerMultiTask('package', 'Handles the creation of minified JS/CSS files', function () {
         var self = this;
         var options = this.options({
@@ -9,21 +12,39 @@ module.exports = function (grunt) {
             clear: []
         });
 
-        // concat the files
-        var src = _.map(self.filesSrc, function (filepath) {
-            if (grunt.file.isDir(filepath)) {
-                return "";
-            }
-            return grunt.file.read(filepath);
-        }).join("\n");
+        if (options.minify === true) {
 
-        // save the concat version
-        grunt.file.write(self.data.dest, src);
-        grunt.log.writeln('File ' + self.data.dest + ' created.');
+            var dest = self.data.dest.trim().toLowerCase();
+            var minify = _.clone(self.data);
+            delete minify.options;
+
+            if (_.endsWith(dest, '.css')) {
+                grunt.config('cssmin.package', minify);
+                grunt.task.run(['cssmin:package']);
+            }
+
+            if (_.endsWith(dest, '.js')) {
+                grunt.config('uglify.package', minify);
+                grunt.task.run(['uglify:package']);
+            }
+
+        } else {
+            // concat the files
+            var src = _.map(self.filesSrc, function (filepath) {
+                if (grunt.file.isDir(filepath)) {
+                    return "";
+                }
+                return grunt.file.read(filepath);
+            }).join("\n");
+
+            // save the concat version
+            grunt.file.write(self.data.dest, src);
+            grunt.log.writeln('File ' + self.data.dest + ' created.');
+        }
 
         // clear unwanted files
-        if(_.isArray(options.clear)) {
-            _.each(options.clear || [],function(filepath){
+        if (_.isArray(options.clear)) {
+            _.each(options.clear || [], function (filepath) {
                 grunt.file.delete(filepath);
             });
         }
