bb8cfe853d1661e535c3759b6905535cee93ce73
unknown <mathew@thinkingmedia.ca>
2016-01-06 10:36:22 -0500

    added coverage reports and opening in the browser

--- tasks/test.js
+++ tasks/test.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var _ = require('lodash');
 
 /**
  * Very simple test runner. Assumes Mocha is being used.
@@ -15,13 +14,15 @@ function Module(grunt) {
 
     // @todo check if mocha is installed via package.json
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.loadNpmTasks('grunt-open');
 
-    grunt.task.registerTask('test', 'Handles executing the tests', function (type) {
-
+    grunt.task.registerTask('test', 'Handles executing the tests', function () {
         var config = {
             test: {
                 options: {
-                    reporter: 'spec'
+                    reporter: 'spec',
+                    captureFile: 'test-results.txt'
                 },
                 src: [
                     c.config().test + '/**/*.test.js'
@@ -36,6 +37,35 @@ function Module(grunt) {
 
         grunt.config('mochaTest', config);
         grunt.task.run(['mochaTest']);
+    });
+
+    grunt.task.registerTask('coverage', 'Generates a test coverage report', function (arg) {
+        var config = {
+            coverage: {
+                options: {},
+                src: [
+                    c.config().test + '/**/*.test.js'
+                ]
+            }
+        };
+
+        var bootstrap = c.config().test + '/bootstrap.js';
+        if (grunt.file.exists(bootstrap)) {
+            config.coverage.options.require = bootstrap;
+        }
+        grunt.config('mocha_istanbul', config);
+
+        if (this.flags.open) {
+            // also open the coverage report when done
+            grunt.config('open', {
+                test: {
+                    path: 'coverage/lcov-report/index.html'
+                }
+            });
+            grunt.task.run(['mocha_istanbul:coverage', 'open:test'])
+        } else {
+            grunt.task.run(['mocha_istanbul:coverage'])
+        }
     });
 }
 
